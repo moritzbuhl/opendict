e207c41ec227391cae44d401d02f6257ff620ee9
mbuhl <git@moritzbuhl.de>
2023-10-27 16:48:07 +0200

    Improve error message.

--- main.c
+++ main.c
@@ -76,7 +76,7 @@ define(struct dc_database *db, struct dc_index_list *l)
 		if (e->match == NULL)
 			break;
 		if ((r = database_lookup(e, db, buf)) == -1) {
-			errx(1, "database_lookup failed for: %.*s\n",
+			errx(1, "dictionary lookup failed for: %.*s\n",
 			    e->match_len, e->match);
 		} else {
 			printf("- %.*s", r, buf);
